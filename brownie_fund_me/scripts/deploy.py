6f0c343580285bed689da19ad723a632470ecdcb
aviralgoel <aviralgoel@rocketmail.com>
2022-05-12 16:46:35 +0400

    added mocks

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,29 +1,29 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import (
+    deploy_mocks,
+    get_account,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+from web3 import Web3
 
 
 def deply_fund_me():
     account = get_account()
-    if network.show_active() != "development":
-        priceFeed_address = config["networks"][network.show.active()][
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        priceFeed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
 
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying mocks...")
-        mocks_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000, {"from": account}
-        )
-        print("Mocks deployed")
-        priceFeed_address = mocks_aggregator.address
+        deploy_mocks()
+        priceFeed_address = MockV3Aggregator[-1].address
 
         # fetch the account details (private key)
 
     fundme = FundMe.deploy(
         priceFeed_address,
         {"from": account},
-        publish_source=True,
+        publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract Deployed to {fundme.address}")
 
